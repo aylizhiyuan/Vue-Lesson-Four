9572b78062d88828eb4f957578a7a0d5683cb68a
lizhiyuan <148334733@qq.com>
2018-10-25 21:25:50 +0800

    主体内容显示出来

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="classObj" class="app-wrapper">
-    <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside"/>
+    <div v-if="device==='mobile' && sidebar.opened" class="drawer-bg" @click="handleClickOutside"/>
     <sidebar class="sidebar-container"/>
     <div class="main-container">
       <navbar/>
@@ -9,9 +9,37 @@
   </div>
 </template>
 <script>
-  
+  import {Navbar,Sidebar,AppMain} from './components'
+  import ResizeMixin from './mixin/ResizeHandler'
   export default {
-    name: "Layout"
+    name: "Layout",
+    components:{
+      Navbar,
+      Sidebar,
+      AppMain
+    },
+    mixins:[ResizeMixin],
+    computed:{
+      sidebar(){
+        return this.$store.state.app.sidebar
+      },
+      device(){
+        return this.$store.state.app.device
+      },
+      classObj(){
+        return {
+          hideSidebar:!this.sidebar.opened,
+          openSidebar:this.sidebar.opened,
+          withoutAnimation:this.sidebar.withoutAnimation,
+          mobile:this.device === 'mobile'
+        }
+      }
+    },
+    methods:{
+      handleClickOutside(){
+        this.$store.dispatch('CloseSideBar',{withoutAnimation:false})
+      }
+    }
   }
 </script>
 <style rel="stylesheet/scss" lang="scss" scoped>
---
+++ src/views/layout/components/Sidebar/SidebarItem.vue
@@ -0,0 +1,14 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+      name: "SidebarItem",
+
+    }
+</script>
+
+<style scoped>
+
+</style>
