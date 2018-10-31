676ac19ff67f77b3e9f134c91a2bd82d8e04cf52
lizhiyuan <148334733@qq.com>
2018-10-31 16:31:45 +0800

    修改了一下request

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,82 +1,73 @@
-//封装一个通用的request方法，用来发请求，使用的时候如下
-/*
-import request from '@/utils/request'
-export function getInfo(params){
-  return request({
-    url:'/user/info',
-    method:'get',
-    params
-  })
-}*/
 import axios from 'axios'
-import { Message } from 'element-ui'
-import store from '@/store'
-import  { getToken } from '@/utils/auth'
+import { Message, MessageBox } from 'element-ui'
+import store from '../store'
+import { getToken } from '@/utils/auth'
 
-//创建一个axios的实例对象
+// 创建axios实例
 const service = axios.create({
-  baseURL:process.env.BASE_API, //api的基地址,在config的dev_env.js里面修改
-  timeout:5000 //请求超时的时间
+  baseURL: process.env.BASE_API, // api 的 base_url
+  timeout: 5000 // 请求超时时间
 })
-//设置请求前的拦截器
+
+// request拦截器
 service.interceptors.request.use(
   config => {
-    if(store.getters.token){
-      //让每一个请求都带着token x-token是自己定义的key
-      config.headers['X-Token'] = getToken()
+    if (store.getters.token) {
+      config.headers['X-Token'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
     }
     return config
   },
   error => {
-    //如果请求错误的话
-    console.log(error)
+    // Do something with request error
+    console.log(error) // for debug
     Promise.reject(error)
   }
 )
-//响应的拦截器
+
+// response 拦截器
 service.interceptors.response.use(
-  response=>{
-    //自定义的错误
+  response => {
+    /**
+     * code为非20000是抛错 可结合自己业务进行修改
+     */
     const res = response.data
-    if(res.code !== 20000){
+    if (res.code !== 20000) {
       Message({
-        message:res.message,
-        type:'error',
-        duration:5*1000
+        message: res.message,
+        type: 'error',
+        duration: 5 * 1000
       })
-      //token验证失败或者是token过期的时候必须拦截处理下
-      //我们可以规定401的错误
-      if(res.code === 401){
-        Message.confirm(
-          '你已被登出,可以取消继续留在该页面,或者重新登录',
+
+      // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
+      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+        MessageBox.confirm(
+          '你已被登出，可以取消继续留在该页面，或者重新登录',
           '确定登出',
           {
-            confirmButtonText:'重新登录',
-            cancelButtonText:'取消登录',
-            type:'warning'
+            confirmButtonText: '重新登录',
+            cancelButtonText: '取消',
+            type: 'warning'
           }
-        ).then(()=>{
-          store.dispatch('FedLogOut').then(()=>{
-            location.reload() //为了重新实例vue-router
+        ).then(() => {
+          store.dispatch('FedLogOut').then(() => {
+            location.reload() // 为了重新实例化vue-router对象 避免bug
           })
         })
       }
       return Promise.reject('error')
-    }else{
-      //如果没有错误的话，就直接结果返回
+    } else {
       return response.data
     }
   },
   error => {
-    //这种错误是拦截时候的错误
-    //为了debug的时候方便
-    console.log('err' + error)
+    console.log('err' + error) // for debug
     Message({
-      message:error.message,
-      type:'error',
-      duration:5 * 1000
+      message: error.message,
+      type: 'error',
+      duration: 5 * 1000
     })
     return Promise.reject(error)
   }
 )
+
 export default service
